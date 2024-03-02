d40f2436c6cd6a5316229decd1be538ff4868f42
Stephen Brennan <stephen.s.brennan@oracle.com>
2024-03-01 16:46:53 -0800

    Implement CTF type and object finder for Linux
    The Compact C Type Format (CTF) is an alternative debug information
    format which describes the types of C data structures. Unlike other
    debug information formats (like DWARF), it is designed to be very
    compact, so that it can be included in production binaries and packages:
    NOT in debuginfo packages.
    
    Support for generating CTF exists in GCC, and support for consuming CTF
    exists in GDB. The Linux kernel does not currently support building with
    CTF, but an effort is underway to change this. Oracle UEK kernels
    already build with CTF: each kernel RPM contains a file named
    "vmlinux.ctfa" which is a CTF archive: it contains type information for
    the kernel and all modules.
    
    CTF can be used as an alternative to DWARF. It cannot provide all the
    features that DWARF would - in particular, stack traces require frame
    pointers, or ORC, or DWARF CFI. CTF cannot allow drgn to read variables
    from stack frames, nor can it provide information about the source code
    files and line numbers associated with program elements. But it can
    provide the necessary information to implement a Type Finder. Assuming
    symbol information is available (e.g. from kallsyms), then an Object
    Finder can be implemented, which looks up names in the symbol table and
    then uses libctf to find the type of the object.
    
    This commit introduces a CTF type & object finder according to this
    approach. To use it, a helper function is added, which allows a user to
    provide the location of the vmlinux.ctfa file.
    
    Signed-off-by: Stephen Brennan <stephen.s.brennan@oracle.com>

---
+++ drgn/helpers/linux/ctf.py
@@ -0,0 +1,59 @@
+# Copyright (c) 2024 Oracle and/or its affiliates
+# SPDX-License-Identifier: LGPL-2.1-or-later
+"""
+CTF
+---
+
+This module contains Python helpers to simplify loading CTF debuginfo and
+associated kallsyms symbol info.
+"""
+import os
+from typing import Optional
+
+from _drgn import _linux_helper_load_ctf
+from drgn import Program
+from drgn.helpers.linux.kallsyms import load_module_kallsyms, load_vmlinux_kallsyms
+
+__all__ = ("load_ctf",)
+
+
+_CTF_PATHS = [
+    "./vmlinux.ctfa",
+    "/lib/modules/{uname}/kernel/vmlinux.ctfa",
+]
+
+
+def load_ctf(
+    prog: Program, path: Optional[str] = None, use_kallsyms: bool = True
+) -> None:
+    """
+    Use Compact Type Format data for debugging.
+
+    This searches the current directory and well-known paths for the
+    "vmlinux.ctfa" file associated with this kernel version. If found, we load
+    the CTF info, and by default, we also load the built-in kallsyms for our
+    symbol table.
+
+    :param prog: Program for debugging
+    :param path: specify an alternative path to ``vmlinux.ctfa``
+    :param use_kallsyms: whether we should try to load kallsyms too
+    """
+    uname = prog["UTS_RELEASE"].string_().decode()
+    if path and not os.path.isfile(path):
+        raise ValueError("CTF Path does not exist")
+    elif not path:
+        for path in _CTF_PATHS:
+            path = path.format(uname=uname)
+            if os.path.isfile(path):
+                break
+        else:
+            raise ValueError(f"Could not find CTF data for {uname}")
+
+    _linux_helper_load_ctf(prog, path)
+
+    if use_kallsyms:
+        finder = load_vmlinux_kallsyms(prog)
+        prog.register_symbol_finder("vmlinux_kallsyms", finder, enable_index=0)
+
+        module_finder = load_module_kallsyms(prog)
+        prog.register_symbol_finder("module_kallsyms", module_finder, enable_index=1)
