0b92f10b3939b52287ad09c249213910933cffc0
Stephen Brennan <stephen.s.brennan@oracle.com>
2024-03-07 10:22:25 -0800

    tests: add CTF testing
    The CTF format needs to be tested, but it is primarily used by the
    kernel, and it is currently out-of-tree in Oracle kernels (but being
    upstreamed). Rather than adding several out-of-tree patches to the
    vmtest subsystem to test kernel CTF directly, a simpler approach is to
    test userspace CTF.
    
    The current drgn CTF implementation isn't generic enough to support
    userspace properly. However, it is good enough to load a simple core
    dump with CTF data, without a symbol table. This gives us everything we
    need to begin testing type representations.
    
    Add a "test.c" file which can be built either with DWARF, GCC's CTF, or
    dwarf2ctf. The two CTF implementations have subtle differences which
    drgn supports, and thus need to each be tested.
    
    Along with this test.c file, add some basic tests. The goal is that
    these tests should pass on both DWARF and CTF. This is not even close to
    a complete test suite, but it does incorporate several areas where the
    CTF implementation needs careful attention: structs with bitfields, and
    multidimensional arrays.
    
    Signed-off-by: Stephen Brennan <stephen.s.brennan@oracle.com>

---
+++ tests/ctfdata/build_dwarf2ctf.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -euxo pipefail
+
+# Drop into a shell if something fails.
+trap 'if [ $? -ne 0 ]; then exec bash -i; fi' EXIT
+
+yum-config-manager --enable ol7_UEKR5
+yum install -y \
+	gcc \
+	pkgconfig \
+	elfutils{,-libelf}-devel \
+	glib2-devel \
+	libdtrace-ctf-devel \
+	zlib-devel \
+	wget
+
+cd /tmp
+wget https://github.com/oracle/linux-uek/raw/uek5/u5/scripts/dwarf2ctf/dwarf2ctf.c \
+     https://github.com/oracle/linux-uek/raw/uek5/u5/scripts/eu_simple.{c,h}
+
+
+CFLAGS="-I. `pkg-config --cflags glib-2.0`"
+LDLIBS="-ldtrace-ctf -lelf -ldw `pkg-config --libs glib-2.0` -lz"
+
+gcc $CFLAGS -o dwarf2ctf dwarf2ctf.c eu_simple.c $LDLIBS
+gcc -g -o tmp /io/$1
+mkdir output
+./dwarf2ctf output -e <(echo tmp)
+cp output/tmp.mod.ctf.new /io/$2
