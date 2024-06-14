3f8040a582923e54809d4bceeb217cae91ab8f81
Stephen Brennan <stephen.s.brennan@oracle.com>
2024-06-14 14:24:08 -0700

    libdrgn: add BTF type & object finder
    A symbol finder should already be loaded, because symbol lookup is
    required to find the __start_BTF and __stop_BTF symbols. The finders can
    then be loaded with:
    
    from _drgn import _linux_helper_load_btf
    _linux_helper_load_btf(prog)
    
    From there, types may be looked up by name, and functions too. Variables
    generally do not have BTF information, so by default that won't work
    well. You could use a function like below to lookup a variable given its
    type:
    
    def var(prog, name, type):
        address = prog.symbol(name).address
        return Object(prog, type, address=address)
    
    From there, a lot of drgn stuff will "just work" because the type
    references are handled transparently. Only when you need to lookup a new
    variable will things stop working again. Sadly, many drgn helpers lookup
    variables by name, and thus the experience is rather broken. You could
    conceivably patch over all of this by creating an object finder based on
    the above var() function, and a large dict mapping global variable names
    to their declared types. A subsequent commit will contain some helpers
    in drgn.helpers.linux.btf to illustrate this.
    
    There are also plenty of limitations to be found in the BTF type &
    object finders, which need to be resolved with future development. None
    are difficult:
    
    1. The BTF parsing has no logic related to endianness. On big-endian
    systems, there could be bugs. Caveat emptor.
    2. The BTF parsing is... basic. There are bound to be bugs. The enum64
    code is fully untested but "should work".
    3. Much of the BTF code is shamefully inefficient. It uses linear search
    for many cases where hashing or binary search would be better.
    4. There are no unit tests.
    
    Signed-off-by: Stephen Brennan <stephen.s.brennan@oracle.com>

--- libdrgn/python/drgnpy.h
+++ libdrgn/python/drgnpy.h
@@ -516,5 +516,7 @@ PyObject *drgnpy_linux_helper_load_ctf(PyObject *self, PyObject *args,
 				       PyObject *kwds);
 PyObject *drgnpy_linux_helper_load_module_ctf(PyObject *self, PyObject *args,
 					      PyObject *kwds);
+PyObject *drgnpy_linux_helper_load_btf(PyObject *self, PyObject *args,
+				       PyObject *kwds);
 
 #endif /* DRGNPY_H */
