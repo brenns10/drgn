828b7f622d84e9580d430a03f3c54c1340b3c471
Stephen Brennan <stephen.s.brennan@oracle.com>
2026-02-10 17:19:40 -0800

    contrib: add script to record variables used
    With BTF support, we need to declare the list of global variables which
    drgn has looked up, in order to provide the type mapping. Add a contrib
    script that can record this using the object finder API, and then print
    them out for later use.
    
    Signed-off-by: Stephen Brennan <stephen.s.brennan@oracle.com>

---
+++ contrib/record_btf_decls.py
@@ -0,0 +1,100 @@
+# Copyright (c) 2026 Oracle and/or its affiliates
+# SPDX-License-Identifier: LGPL-2.1-or-later
+import argparse
+import os
+import pkgutil
+import runpy
+import sys
+from typing import Optional
+
+from drgn import FindObjectFlags, Program, Type, TypeKind
+from drgn.cli import run_interactive
+
+
+def record_object_lookups(prog: Program) -> None:
+    if "record_object_lookups" in prog.registered_object_finders():
+        return
+
+    lookups = set()
+
+    def ofind(prog, name, flags, filename) -> None:
+        if flags & FindObjectFlags.VARIABLE:
+            lookups.add((name, filename))
+        return None
+
+    # Register at index 1 so that we don't observe the "linux_kernel" object
+    # finder lookups, but we see everything else.
+    prog.register_object_finder("record_object_lookups", ofind, enable_index=1)
+    prog.cache["record_object_lookups"] = lookups
+
+
+def _format_type(tp: Type, name: Optional[str]) -> str:
+    if tp.kind in (TypeKind.STRUCT, TypeKind.UNION, TypeKind.ENUM, TypeKind.CLASS):
+        if not tp.tag:
+            raise ValueError(f"cannot reference anonymous {tp.kind.name.lower()}")
+        if name:
+            return f"{tp.kind.name.lower()} {tp.tag} {name}"
+        else:
+            return f"{tp.kind.name.lower()} {tp.tag}"
+    elif tp.kind == TypeKind.POINTER:
+        return _format_type(tp.type, f"*{name}")
+    elif tp.kind == TypeKind.ARRAY:
+        if tp.length is None:
+            return _format_type(tp.type, f"{name}[]")
+        else:
+            return _format_type(tp.type, f"{name}[{tp.length}]")
+    elif tp.kind == TypeKind.FUNCTION:
+        raise NotImplementedError("function declarations are not implemented")
+    elif name:
+        return f"{tp.name} {name}"
+    else:
+        return tp.name
+
+
+def get_object_declarations(prog: Program) -> str:
+    # Include the minimum modules + btf_modules for any generated result
+    decls = [
+        "struct list_head modules, btf_modules;"
+    ]
+    for name, filename in prog.cache["record_object_lookups"]:
+        try:
+            result = prog.variable(name, filename)
+        except LookupError:
+            continue
+        try:
+            decls.append(_format_type(result.type_,name) + ";")
+        except ValueError as e:
+            decls.append(f"// {name}: {e}")
+    return "\n".join(decls)
+
+
+def main(prog: Program):
+    parser = argparse.ArgumentParser(
+        description="run script or interactive, recording variables used"
+    )
+    parser.add_argument("-o", "--output", type=str, help="file to store declarations")
+    args, remainder = parser.parse_known_args()
+    record_object_lookups(prog)
+    try:
+        if remainder:
+            sys.argv = remainder
+            script_path = sys.argv[0]
+            if pkgutil.get_importer(script_path) is None:
+                sys.path.insert(0, os.path.dirname(os.path.abspath(script_path)))
+            runpy.run_path(
+                script_path, init_globals={"prog": prog}, run_name="__main__"
+            )
+        else:
+            run_interactive(prog)
+    finally:
+        decls = get_object_declarations(prog)
+        if args.output:
+            with open(args.output, "w") as f:
+                f.write(decls)
+        else:
+            print("==== DECLARATIONS ====")
+            print(decls)
+
+
+if __name__ == "__main__":
+    main(prog)  # noqa
